14b567f8eae5407aca98e579860b7854785e9ae4
shweta hasani <95736603+Shwetahasani@users.noreply.github.com>
2023-09-02 11:08:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+// Date dynamic
+const endDate = " 10 september 2023 3:35 PM"
+document.getElementById("end-date").innerHTML=endDate;
+const inputs = document.querySelectorAll("input");
+function clock(){
+    const end =new  Date(endDate);
+    const now= new Date();
+    
+    const diff= (end -now)/1000;
+   if(diff<0){
+    return;
+   }
+   inputs[0].value= Math.floor(diff/3600/24);
+   inputs[1].value= Math.floor(diff/3600)%24;
+   inputs[2].value= Math.floor(diff/60)%60;
+   inputs[3].value= Math.floor(diff)%60;
+   
+}
+clock();
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
